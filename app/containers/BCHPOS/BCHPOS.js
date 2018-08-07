a164e6068e389eb19407a533f69de2858c5f4b69
lightswarm124 <lightswarm@gmail.com>
2018-08-07 15:50:43 -0400

    mirrored container format for BCHPOS folder

---
+++ app/containers/BCHPOS/BCHPOS.js
@@ -0,0 +1,16 @@
+/*
+ * BCHPOS
+ *
+ * This is the page for the POS
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import './style.scss';
+
+export default class BCHPOS extends React.PureComponent {
+
+}
+
+// BCHPOS.propTypes = {};
