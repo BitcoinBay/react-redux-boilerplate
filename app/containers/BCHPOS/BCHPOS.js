ae7897f7588a02cb3d52a3b8db5a4744b49b8c45
lightswarm124 <lightswarm@gmail.com>
2018-08-08 12:33:30 -0400

    Cashier Page w/ NumPad

--- app/containers/BCHPOS/BCHPOS.js
+++ app/containers/BCHPOS/BCHPOS.js
@@ -7,10 +7,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
+import NumPad from 'react-numpad';
+
 import './style.scss';
 
 export default class BCHPOS extends React.PureComponent {
-
+  render() {
+    return (
+      <div className="cashier-page">
+        <Helmet>
+          <title>Cashier Page</title>
+          <meta
+            name="description"
+            content="Cashier page of React.js BCHPOS application"
+          />
+        </Helmet>
+        <h1>Cashier Page</h1>
+        <NumPad.Number
+          onChange={(value) => { console.log('value', value)}}
+          label={'Total'}
+          placeholder={'my placeholder'}
+          value={100}
+        />
+      </div>
+    );
+  }
 }
 
 // BCHPOS.propTypes = {};
